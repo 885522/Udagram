14fee268c4c90a2e874b3046f1a6934f54d62ed3
885522 <omaraboulmakarem@gmail.com>
2022-05-04 22:35:20 +0200

    try

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0',
+  apiHost: 'Udagram-env.eba-asy4g2u7.eu-west-1.elasticbeanstalk.com',
 };
 
 /*
